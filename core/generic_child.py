a72bd5e92575dd54f349a6db15551fe57bd7587e
khiyam <khiyamiftikhar@gmail.com>
2026-02-17 14:38:04 +0500

    Screen load methods added src and header

--- core/generic_child.py
+++ core/generic_child.py
@@ -48,8 +48,10 @@ class ChildSpec:
         job_struct = f"ui_{screen_snake}_{self.type_name.lower()}_job_t"
 
         return load_template(self.setter_template).format(
+            #sc_fn_name=f"ui_{screen_snake}_load",
             fn_name=f"ui_{screen_snake}_set_{child.id}",
             cb_name=f"ui_{screen_snake}_set_{child.id}_job",
+            #sc_fn_cb_name=f"ui_{screen_snake}_load_job",
             job_struct=job_struct,
             child_index=index,
         )
