f818e4e1bfcd78f9a3429f82aac0ae92ebc7497f
khiyam <khiyamiftikhar@gmail.com>
2026-02-18 11:33:40 +0500

    working

--- examples/simple/main/main.c
+++ examples/simple/main/main.c
@@ -1,14 +1,45 @@
 
+
 #include "lcd_device.h"
 #include "ui_home.h"
 #include "ui_boot.h"
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
+#define DELAY_MS 3000
 
 
 void app_main(void)
 {
-    lcd_init();
+    esp_err_t ret;
+    ret = lcd_init();
+    if (ret != ESP_OK) {
+        //ESP_LOGE("main", "Failed to initialize LCD");
+        return;
+    }
+
     ui_boot_init();
     ui_home_init();
+
+    ui_boot_screen_set_label_main("Booting Scr");
+    vTaskDelay(pdMS_TO_TICKS(DELAY_MS)); // Simulate some delay for the boot screen
+    ui_boot_screen_set_label_channel_no("Ch 1");
+    vTaskDelay(pdMS_TO_TICKS(DELAY_MS)); // Simulate some delay for the boot screen
+    ui_boot_screen_set_label_discovery("Active");
+
+    ui_boot_screen_load();
+
+    vTaskDelay(pdMS_TO_TICKS(DELAY_MS)); // Simulate some delay for the boot screen
+    ui_home_screen_set_label_ap_ssid("MyNet");
+    vTaskDelay(pdMS_TO_TICKS(DELAY_MS)); // Simulate some delay for the boot screen
+    ui_home_screen_set_label_main("Home Screen");
+    vTaskDelay(pdMS_TO_TICKS(DELAY_MS)); // Simulate some delay for the boot screen
+    ui_home_screen_set_icon_wifi(1);
+    vTaskDelay(pdMS_TO_TICKS(DELAY_MS)); // Simulate some delay for the boot screen
+    ui_home_screen_set_label_discovery("Complete");
+    vTaskDelay(pdMS_TO_TICKS(DELAY_MS)); // Simulate some delay for the boot screen
+    ui_home_screen_load();
+
+    
 }
