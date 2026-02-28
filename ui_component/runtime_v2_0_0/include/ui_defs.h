7cabb0aa7f85395fa84524a01b262946fe623f41
khiyam <khiyamiftikhar@gmail.com>
2026-02-28 09:33:20 +0500

    new redefined ui_def and worker used

--- ui_component/runtime_v2_0_0/include/ui_defs.h
+++ ui_component/runtime_v2_0_0/include/ui_defs.h
@@ -18,7 +18,9 @@ typedef enum
 {
     UI_CHILD_ICON,
     UI_CHILD_LABEL,
-    UI_CHILD_BAR
+    UI_CHILD_BAR,
+    UI_CHILD_IMAGE,
+    
 } ui_child_type_t;
 
 // Multi-state icon description
@@ -27,31 +29,38 @@ typedef struct {
     const void* state_src[UI_MAX_ICON_STATES];   // pointer to arrays OR file paths
 } ui_icon_t;
 
-// A child object of a screen
 typedef struct {
-    ui_child_type_t type;
-    char id[UI_MAX_ID_LENGTH];      //To differentiate among elements of same type e.g labels, whether main or header
 
-    // LVGL object handle created at runtime
+    ui_child_type_t type;
+    char id[UI_MAX_ID_LENGTH];
+
     lv_obj_t *lv_obj;
 
-    // Generic attributes
     int x;
     int y;
     int w;
     int h;
 
-    // For ICON type
-    ui_icon_t *icon;
-    uint8_t current_state;
+    union {
 
-    // For LABEL type
-    char text[UI_MAX_STRING_LENGTH];
+        struct {    // LABEL
+            char text[UI_MAX_STRING_LENGTH];
+        } label;
 
-    // For BAR type
-    int initial_value;
+        struct {    // BAR
+            int32_t initial_value;
+            int32_t min;
+            int32_t max;
+        } bar;
+
+        struct {
+            const lv_image_dsc_t *src;
+        } image;
+
+
+    } data;
+
 } ui_child_t;
-
 // A complete screen
 typedef struct {
     const char *name;
@@ -61,6 +70,47 @@ typedef struct {
     lv_obj_t *lv_screen;     // created at runtime
 } ui_screen_t;
 
+
+
+
+typedef struct ui_job_t ui_job_t;
+typedef void (*ui_job_cb_t)(ui_job_t *job);
+
+typedef enum {
+    UI_JOB_SET_LABEL,
+    UI_JOB_LOAD_SCREEN,
+    UI_JOB_SET_BAR,
+    UI_JOB_SET_IMAGE,
+} ui_job_type_t;
+
+typedef struct {
+    
+    char text[UI_MAX_STRING_LENGTH];
+} ui_label_job_t;
+
+
+typedef struct {
+    int32_t value;        // Target value
+    uint32_t duration_ms; // Animation duration
+} ui_bar_job_t;
+
+typedef struct {
+    const lv_image_dsc_t *src;   // pointer to compiled image
+} ui_image_job_t;
+
+
+typedef struct ui_job_t {
+    ui_job_cb_t cb;
+    uint8_t child_index;
+    ui_job_type_t type;
+
+    union {
+        ui_label_job_t label;
+        ui_bar_job_t   bar;
+        ui_image_job_t image;
+    } data;
+
+} ui_job_t;
 
 #ifdef __cplusplus
 }
--- ui_component/runtime_v2_0_0/src/ui_worker.c
+++ ui_component/runtime_v2_0_0/src/ui_worker.c
@@ -1,36 +1,37 @@
 // lvgl_port_notify.c
-
-#include <string.h>
 #include "ui_worker.h"
 #include "esp_lvgl_port.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/queue.h"
 
-#define UI_WORKER_MAX_JOB_SIZE    64
-
 typedef struct {
-    void (*cb)(void *args);
-    uint16_t data_size;
-    uint8_t data[UI_WORKER_MAX_JOB_SIZE];
+    void (*cb)(void* args);
+    void *user_data;
 } notify_msg_t;
-
 
 static QueueHandle_t notify_queue = NULL;
 
 static void ui_worker_task(void *arg)
 {
-    notify_msg_t msg;
+    ui_job_t job;
 
     while (1) {
-        if (xQueueReceive(notify_queue, &msg, portMAX_DELAY)) {
 
+        if (xQueueReceive(notify_queue, &job, portMAX_DELAY)) {
+
+            // SAFELY perform UI update
             if (lvgl_port_lock(portMAX_DELAY)) {
 
-                msg.cb((void *)msg.data);
+                if (job.cb != NULL) {
+                    job.cb(&job);
+                }
 
                 lvgl_port_unlock();
             }
+
+            // If still using esp_lvgl_port and needed:
+            // lvgl_port_task_wake();
         }
     }
 }
@@ -38,36 +39,18 @@ static void ui_worker_task(void *arg)
 // --------------------------------------------------------
 // PUBLIC API
 // --------------------------------------------------------
-//Not thread safe, assuming that the init part of formware is single threaded.
 void ui_worker_init(void)
 {
-    if(notify_queue != NULL) {
-        // already initialized
-        return;
-    }
-    notify_queue = xQueueCreate(16, sizeof(notify_msg_t));
+    notify_queue = xQueueCreate(16, sizeof(ui_job_t));
     ESP_ERROR_CHECK(notify_queue==NULL);
 
-    BaseType_t  ret=xTaskCreate(ui_worker_task, "lvgl_notify", 2048, NULL, 5, NULL);
+    BaseType_t  ret=xTaskCreate(ui_worker_task, "lvgl_notify", 4096, NULL, 5, NULL);
     ESP_ERROR_CHECK(ret!=pdTRUE);
 }
 
-bool ui_worker_process_job(void (*cb)(void *),
-                           const void *data,
-                           uint16_t size){
-    notify_msg_t msg;
-
-    msg.cb = cb;
-    msg.data_size = size;
-
-    if (size > UI_WORKER_MAX_JOB_SIZE) {
-        // optionally assert / log
-        return false;
-    }
-
-    memcpy(msg.data, data, size);
-
-    return xQueueSend(notify_queue, &msg, portMAX_DELAY);
+bool ui_worker_post_job(const ui_job_t *job)
+{
+    return xQueueSend(notify_queue, job, 0) == pdTRUE;
 }
 
 bool ui_worker_process_job_sync(void (*cb)(void* args), void *user_data)
